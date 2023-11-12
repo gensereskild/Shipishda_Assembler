bb1167fe58bebb2da18b4a0be927782e463f1ab9
Fleli <frederikedvardsen@gmail.com>
2023-11-12 13:45:49 +0100

    Added JavaDoc to the Token file

--- Lexer/Token.java
+++ Lexer/Token.java
@@ -4,8 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * A {@code Token} represents a lexical word, for example an identifier, a number, or a register.
+ */
 public class Token {
     
+    /**
+     * These are the legal token types. If a token is initialized with a type that isn't among these,
+     * the constructor will throw an exception.
+     */
     private static final List<String> legalTypes = new ArrayList<>(Arrays.asList(
         
     ));
